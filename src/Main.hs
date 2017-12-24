a0cfc9377f9957e609fa233dfbf38f2135054107
Lae <laec@protonmail.ch>
2017-12-24 16:07:26 +1300

    Print some info.

--- src/Main.hs
+++ src/Main.hs
@@ -2,25 +2,27 @@
 
 module Main where
 
-import           Game
-import           Game.Database
-import           Game.Store
-import           Network.HTTP.Client.TLS
-import           System.Environment
-import           System.Exit
+import Game
+import Game.Database
+import Game.Store
+import Network.HTTP.Client.TLS
+import System.Environment
+import System.Exit
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [dbFile] -> process dbFile
-    _        -> die "Usage: <this-program> <db-json>"
+    _ -> die "Usage: <this-program> <db-json>"
 
 process :: FilePath -> IO ()
 process dbFile = do
-  db <- readDbIfExists dbFile >>= \case
-    Right db -> return db
-    Left err -> die err
-  manager <- newTlsManager
-  games <- downloadGames manager
+  db <-
+    readDbIfExists dbFile >>= \case
+      Right db -> return db
+      Left err -> die err
+  putStrLn "Downloading game data..."
+  games <- downloadGames =<< newTlsManager
+  putStrLn $ "Downloaded data for " ++ show (length games) ++ " games."
   writeDb dbFile (update db games)
