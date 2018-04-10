7e05d085cf5d66f79de70e7993c842e766901396
Lae <laec@protonmail.ch>
2018-04-10 23:09:09 +1200

    Refactor.

--- src/Game/Store.hs
+++ src/Game/Store.hs
@@ -19,13 +19,12 @@ import Data.Time.Clock
 import Game
 import Network.HTTP.Client
 import Pipes
-import Pipes.Core
 import System.Log.Logger
 
 parseGames :: Day -> Value -> Parser [Game]
 parseGames date =
   withObject "Games" $ \v ->
-    mapMaybe (parseMaybe (parseGame date) . Object) <$> (v .: "included")
+    mapMaybe (parseMaybe $ parseGame date) <$> v .: "included"
 
 parseGame :: Day -> Value -> Parser Game
 parseGame date =
@@ -33,10 +32,10 @@ parseGame date =
     sku <- strip <$> v .: "id"
     attrs <- v .: "attributes"
     name <- strip <$> attrs .: "name"
-    releaseDate <- utctDay <$> (attrs .: "release-date")
+    releaseDate <- utctDay <$> attrs .: "release-date"
     platforms <- sort . map strip <$> attrs .: "platforms"
     prices <- attrs .: "default-sku-id" >>= parsePrices attrs
-    return
+    pure
       Game
       { sku = sku
       , name = name
@@ -48,23 +47,22 @@ parseGame date =
 parsePrices :: Object -> Text -> Parser Prices
 parsePrices attrs sku = do
   skus <- attrs .: "skus" :: Parser [Object]
-  prices <-
-    maybe (fail "No matching sku.") pure (find (matches sku) skus) >>=
-    (.: "prices") >>=
-    (.: "non-plus-user")
-  actual <- parsePrice "actual-price" prices
-  upsell <- parsePrice "upsell-price" prices
-  strikethrough <- parsePrice "strikethrough-price" prices
-  return
-    Prices {actual = actual, upsell = upsell, strikethrough = strikethrough}
+  prices <- search skus sku
+  actual <- parse prices "actual-price"
+  upsell <- parse prices "upsell-price"
+  strike <- parse prices "strikethrough-price"
+  pure Prices {actual = actual, upsell = upsell, strikethrough = strike}
   where
-    matches id o = HM.lookup "id" o == Just (String id)
-    parsePrice tag prices = prices .:? tag >>= maybe (pure Nothing) (.: "value")
+    parse prices tag = prices .:? tag >>= maybe (pure Nothing) (.: "value")
+    search skus sku =
+      let found = maybe (fail "sku not found") pure $ find matching skus
+          matching o = HM.lookup "id" o == Just (String sku)
+      in found >>= (.: "prices") >>= (.: "non-plus-user")
 
 url :: Int -> String
-url start =
+url offset =
   "https://store.playstation.com/valkyrie-api/en/NZ/999/container/STORE-MSF75508-FULLGAMES?size=500&bucket=games&start=" ++
-  show start
+  show offset
 
 games :: Manager -> Producer Game IO ()
 games = games' 0
@@ -74,7 +72,7 @@ games' offset manager = do
   today <- utctDay <$> lift getCurrentTime
   content <- lift $ readContent manager offset
   items <- either fail pure (parse content today)
-  unless (null items) $ each items //> yield >> next items
+  unless (null items) $ for (each items) yield >> next items
   where
     parse content date = eitherDecode content >>= parseEither (parseGames date)
     next items = games' (offset + length items) manager
